05de73f697d6b3e7b61f23b1c48147ca3d9dde7c
Anton Bushuiev <anton.bushuiev@gmail.com>
2023-11-22 09:03:48 +0100

    Minor fixex

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name='mutils',
     version='0.0',
-    packages=find_packages(),
-    install_requires=required,
+    packages=find_packages()
 )
